7c8b1814fdcf295192e44cc872c72307bf89cdaa
leojo <leojohannsson91@gmail.com>
2016-11-23 11:11:05 +0100

    Changed xval_test scoring to mirror score on kaggle

--- Submissions/submit_adaboost.py
+++ Submissions/submit_adaboost.py
@@ -3,9 +3,9 @@ import numpy as np
 from sklearn import pipeline
 from sklearn.feature_selection import *
 from sklearn.preprocessing import *
+from sklearn.decomposition import *
 from sklearn.linear_model import *
 from sklearn.ensemble import *
-from sklearn.svm import *
 from sklearn.model_selection import cross_val_score
 
 from Features.extract_features import *
@@ -21,7 +21,9 @@ histograms = np.array(extractHistograms("../data/set_train",4500,45,9))
 
 print "Training model"
 model = pipeline.make_pipeline(
-		SelectKBest(k=1500),
+		StandardScaler(),
+		SelectKBest(k=1400),
+		PCA(n_components=100),
 		AdaBoostClassifier()
 	)
 model.fit(histograms,targets)
@@ -30,7 +32,7 @@ print "Testing model"
 testData = np.array(extractHistograms('../data/set_test',4500,45,9))
 predictions = model.predict(testData).flatten().tolist()
 print predictions
-with open('ababoost.csv', 'w') as csvfile:
+with open('adaboost.csv', 'w') as csvfile:
 	resultWriter = csv.writer(csvfile, delimiter=',', quotechar='|')
 	resultWriter.writerow(['ID','Prediction'])
 	for i in range(0,len(predictions)):
