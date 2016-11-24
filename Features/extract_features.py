ece2b774ab24fdb54d705ad3036a93eb2a008cb2
andreabjork <andreabjork43@gmail.com>
2016-11-24 13:18:46 +0100

    some experimentation with different colored zones

--- Features/extract_features.py
+++ Features/extract_features.py
@@ -306,6 +306,108 @@ def extractZoneAverages(imgDir, nPartitions = 1):
 
 	return allZoneAverages
 
+def extractBlackzones(imgDir, nPartitions=1):
+	imgPath = os.path.join(imgDir,"*")
+
+	allBlackZones = []
+
+	# This is the cache for the feature, used to make sure we do the heavy computations more often than neccesary
+	outputFileName = os.path.join(featuresDir,"blackzones_"+str(nPartitions)+"_"+imgDir.replace(os.sep,"-")+".feature")
+	if os.path.isfile(outputFileName):
+		save = open(outputFileName,'rb')
+		zoneAverages = pickle.load(save)
+		save.close()
+		return zoneAverages
+
+	# Fetch all directory listings of set_train and sort them on the image number
+	allImageSrc = sorted(glob.glob(imgPath), key=extractImgNumber)
+	n_samples = len(allImageSrc);
+	print "Found "+str(n_samples)+" images!"
+	print "Preparing the data"
+	printProgress(0, n_samples)
+	for i in range(0,n_samples):
+		img = nib.load(allImageSrc[i])
+		imgData = img.get_data();
+		imgDataDisected = imgData[20:148, 35:163, 30:158, 0]
+
+		blackzones = np.asarray([[[0]*nPartitions]*nPartitions]*nPartitions)
+		# Size should be same for all dimensions, imgData should
+		# have same dimensions for x, y, z all such that they can be
+		# divided by nPartitions
+		for x in range(imgDataDisected.shape[0]):
+			for y in range(imgDataDisected.shape[1]):
+				for z in range(imgDataDisected.shape[2]):
+					val = imgDataDisected[x][y][z]
+					partX = int((x*nPartitions)/imgDataDisected.shape[0])
+					partY = int((y*nPartitions)/imgDataDisected.shape[1])
+					partZ = int((z*nPartitions)/imgDataDisected.shape[2])
+					if val < 450 and val > 0:
+						blackzones[partX][partY][partZ] += 1
+
+		allBlackZones.append(blackzones.flatten().tolist())
+		printProgress(i+1, n_samples)		
+		
+
+	print "\nStoring the features in "+outputFileName
+	output = open(outputFileName,"wb")
+	pickle.dump(allBlackZones,output)
+	output.close()
+	print "Done"
+	return allBlackZones
+
+
+# Written for 2D for faster computing while testing
+def extractColoredZone(imgDir, minColor, maxColor, nPartitions=1):
+	imgPath = os.path.join(imgDir,"*")
+
+	allColoredZones = []
+
+	# This is the cache for the feature, used to make sure we do the heavy computations more often than neccesary
+	outputFileName = os.path.join(featuresDir,"coloredzones_"+str(nPartitions)+"_"+str(minColor)+"_"+str(maxColor)+"_"+imgDir.replace(os.sep,"-")+".feature")
+	if os.path.isfile(outputFileName):
+		save = open(outputFileName,'rb')
+		zoneAverages = pickle.load(save)
+		save.close()
+		return zoneAverages
+
+	# Fetch all directory listings of set_train and sort them on the image number
+	allImageSrc = sorted(glob.glob(imgPath), key=extractImgNumber)
+	n_samples = len(allImageSrc);
+	print "Found "+str(n_samples)+" images!"
+	print "Preparing the data"
+	printProgress(0, n_samples)
+	for i in range(0,n_samples):
+		img = nib.load(allImageSrc[i])
+		imgData = img.get_data();
+		imgDataDisected = imgData[20:148, 35:163, 88, 0]
+
+		colZones = np.asarray([[[0]*nPartitions]*nPartitions]*nPartitions)
+		# Size should be same for all dimensions, imgData should
+		# have same dimensions for x, y, z all such that they can be
+		# divided by nPartitions
+		for x in range(imgDataDisected.shape[0]):
+			for y in range(imgDataDisected.shape[1]):
+				#for z in range(imgDataDisected.shape[2]):
+					val = imgDataDisected[x][y]#[z]
+					partX = int((x*nPartitions)/imgDataDisected.shape[0])
+					partY = int((y*nPartitions)/imgDataDisected.shape[1])
+					#partZ = int((z*nPartitions)/imgDataDisected.shape[2])
+					if val <= maxColor and val >= minColor:
+						colZones[partX][partY] += 1
+
+		allColoredZones.append(colZones.flatten().tolist())
+		printProgress(i+1, n_samples)		
+		
+
+	print "\nStoring the features in "+outputFileName
+	output = open(outputFileName,"wb")
+	pickle.dump(allColoredZones,output)
+	output.close()
+	print "Done"
+	return allColoredZones
+
+
+
 def extractImgNumber(imgPath):
 	imgName = imgPath.split(os.sep)[-1]
 	imgNum = int(imgName.split('_')[-1][:-4])
